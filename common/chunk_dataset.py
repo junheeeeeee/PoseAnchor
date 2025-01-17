9dbd8f76ba50b32e2dc698e87d8c89fc2957cfc3
Juni <jh__kim@korea.ac.kr>
2025-01-17 13:15:20 +0900

    main

--- common/chunk_dataset.py
+++ common/chunk_dataset.py
@@ -98,8 +98,9 @@ class ChunkedDataset_Seq(Dataset):
         # 2D poses 추출
         # ----------------------------
         seq_2d = self.poses_2d[seq_i]
-        start_2d = start_3d  # pad, causal_shift 반영하려면 추가할 수도 있음
-        end_2d   = end_3d
+        random_shift = np.random.randint(-20, 20)
+        start_2d = start_3d + random_shift  # pad, causal_shift 반영하려면 추가할 수도 있음
+        end_2d   = end_3d + random_shift
 
         low_2d  = max(start_2d, 0)
         high_2d = min(end_2d, seq_2d.shape[0])
