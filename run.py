4bb34865315533890b4ebfe1f76828554c17d8bc
3090 <jh__kim@korea.ac.kr>
2025-02-05 17:49:12 +0900

    main

--- run.py
+++ run.py
@@ -678,7 +678,7 @@ def main():
                     wandb.log({"3d_train": losses_3d_train[-1] * 1000}, epoch+1, commit=True)
             # Decay learning rate exponentially
             lr *= lr_decay
-            if epoch >= 90:
+            if epoch >= 30:
                 for param_group in optimizer.param_groups:
                     param_group['lr'] *= lr_decay
             epoch += 1
